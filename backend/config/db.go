00deea3776038646a5452d33d751fdc668c4eb87
myfarism <muhammad.farishafizh@student.upj.ac.id>
2026-02-24 03:53:04 +0700

    UPDATE database connection settings to require SSL and enhance error logging

--- backend/config/db.go
+++ backend/config/db.go
@@ -13,7 +13,7 @@ var DB *sqlx.DB
 
 func InitDB() {
     dsn := fmt.Sprintf(
-        "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+        "host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
         os.Getenv("DB_HOST"),
         os.Getenv("DB_PORT"),
         os.Getenv("DB_USER"),
@@ -23,9 +23,15 @@ func InitDB() {
 
     db, err := sqlx.Connect("postgres", dsn)
     if err != nil {
-        log.Fatalf("Failed to connect to database: %v", err)
+        log.Fatalf("[DB] Failed to connect: %v\nDSN: host=%s port=%s user=%s dbname=%s",
+            err,
+            os.Getenv("DB_HOST"),
+            os.Getenv("DB_PORT"),
+            os.Getenv("DB_USER"),
+            os.Getenv("DB_NAME"),
+        )
     }
 
     DB = db
-    log.Println("Database connected")
+    log.Println("[DB] Connected successfully")
 }
